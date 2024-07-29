194a2e076d877a3127cf35c4bac8dbe74261ec02
DavidLucasK <davidlucasfr70@gmail.com>
2024-07-29 10:27:28 -0300

    migrando projeto para supabase

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Funções comuns para ambas as páginas
     const validateEmail = (email) => {
         const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         return re.test(email);
@@ -8,71 +7,60 @@ document.addEventListener('DOMContentLoaded', () => {
     const validatePassword = (password) => {
         const errors = [];
 
-        // Verifica o comprimento mínimo
         if (password.length < 6) {
             errors.push('A senha deve ter pelo menos 6 caracteres.');
         }
 
-        // Verifica se a senha contém pelo menos 2 números
         const numberCount = (password.match(/\d/g) || []).length;
         if (numberCount < 2) {
             errors.push('A senha deve conter pelo menos 2 números.');
         }
 
-        // Verifica se a senha contém pelo menos 1 caractere especial
         const specialCharRegex = /[!@#$%^&*(),.?":{}|<>]/;
         if (!specialCharRegex.test(password)) {
             errors.push('A senha deve conter pelo menos 1 caractere especial.');
         }
 
-        // Retorna o array de mensagens de erro
         return errors;
     };
 
-    // Função para lidar com a criação de conta
     const handleSignUp = async () => {
         const email = document.getElementById('signupEmail').value.trim();
         const password = document.getElementById('signupPassword').value.trim();
         const resultDiv = document.getElementById('signupResult');
 
-        // Valida a senha
         const errors = validatePassword(password);
 
-        // Se houver erros, exibe a primeira mensagem de erro
         if (errors.length > 0) {
-            resultDiv.innerHTML = errors[0]; // Exibe apenas a primeira mensagem de erro
+            resultDiv.innerHTML = errors[0];
             resultDiv.style.color = 'red';
             return;
         }
 
-        // Verifica se o email é válido
         if (!validateEmail(email)) {
             resultDiv.textContent = 'Email inválido.';
             resultDiv.style.color = 'red';
             return;
         }
 
-        // Chamada para a API de registro
         try {
-            const response = await fetch('https://loginbackend-2ff9.onrender.com/api/auth/register', {
+            const response = await fetch('https://https://ojxyfmbpzjypidukzlqf.supabase.co/auth/v1/signup', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
+                    'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9qeHlmbWJwemp5cGlkdWt6bHFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIyNTc5NjIsImV4cCI6MjAzNzgzMzk2Mn0._iRG2YBG6bRkYZG27BRbD-KnrAX1aBHqloTvHGlcNKQ'
                 },
                 body: JSON.stringify({ email, password }),
             });
 
             const data = await response.json();
-            resultDiv.textContent = data.message;
+            resultDiv.textContent = data.message || 'Conta criada com sucesso!';
             resultDiv.style.color = response.ok ? 'green' : 'red';
 
-            // Adicionando console.log para verificar a resposta
-            console.log('Resposta da API de registro:', data);
-
             if (response.ok) {
-                console.log('Usuário inserido no banco de dados com sucesso:', data.userID);
+                console.log('Usuário registrado na Supabase:', data);
                 setTimeout(() => {
-                    window.location.href = 'index.html'; // Redireciona após 2 segundos
+                    window.location.href = 'index.html';
                 }, 2000);
             }
         } catch (err) {
@@ -82,28 +70,28 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     };
 
-    // Função para lidar com o login
     const handleLogin = async () => {
         const emailInput = document.querySelector('input[type="string"]').value.trim();
         const passwordInput = document.querySelector('input[type="password"]').value.trim();
         const resultDiv = document.getElementById('result');
 
         try {
-            const response = await fetch('https://loginbackend-2ff9.onrender.com/api/auth/login', {
+            const response = await fetch('https://https://ojxyfmbpzjypidukzlqf.supabase.co/auth/v1/token?grant_type=password', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
+                    'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9qeHlmbWJwemp5cGlkdWt6bHFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIyNTc5NjIsImV4cCI6MjAzNzgzMzk2Mn0._iRG2YBG6bRkYZG27BRbD-KnrAX1aBHqloTvHGlcNKQ'
                 },
                 body: JSON.stringify({ email: emailInput, password: passwordInput }),
             });
 
             const data = await response.json();
-            resultDiv.textContent = data.message;
+            resultDiv.textContent = data.message || 'Login bem-sucedido!';
             resultDiv.style.color = response.ok ? 'green' : 'red';
 
             if (response.ok) {
-                console.log('Login bem-sucedido, token recebido:', data.token);
-                localStorage.setItem('authToken', data.token); // Armazena o token no localStorage
+                console.log('Login bem-sucedido, token recebido:', data.access_token);
+                localStorage.setItem('authToken', data.access_token);
                 setTimeout(() => {
                     window.location.href = 'https://davidlucas.vercel.app';
                 }, 2000);
@@ -115,13 +103,11 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     };
 
-    // Página de Login
     if (document.getElementById('convertBtn')) {
         const loginButton = document.getElementById('convertBtn');
         loginButton.addEventListener('click', handleLogin);
     }
 
-    // Página de Criação de Conta
     if (document.getElementById('signupBtn')) {
         const signupButton = document.getElementById('signupBtn');
         signupButton.addEventListener('click', handleSignUp);
