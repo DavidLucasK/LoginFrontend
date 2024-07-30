7166e2e666a85aea21e4e6eadfd013f7da42a3ee
DavidLucasK <davidlucasfr70@gmail.com>
2024-07-30 01:21:21 -0300

    href home

--- scripts/reset.js
+++ scripts/reset.js
@@ -59,6 +59,9 @@ async function handleResetPassword(event) {
 
         if (response.ok) {
             document.getElementById('result').textContent = 'Senha redefinida com sucesso!';
+            setTimeout(() => {
+                window.location.href = 'index.html';
+            }, 2000);
         } else {
             document.getElementById('result').textContent = data.message || 'Erro ao redefinir a senha.';
         }
